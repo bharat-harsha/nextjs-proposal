e0ad5047b01127634915f11534afb96bc8d134db
bharath-harsha <bharath.harsha@techpearl.com>
2023-10-03 15:30:04 +0530

    sample proposal route

---
+++ app/proposals/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProposalInfo = () => {
+  return (
+    <div>
+        <h1>Sustainable Renewable Energy</h1>
+        <p>According to the World Health Organization (WHO), about 99 percent of people in the world breathe air that exceeds air quality limits and threatens their health, and more than 13 million deaths around the world each year are due to avoidable environmental causes, including air pollution.
+          The unhealthy levels of fine particulate matter and nitrogen dioxide originate mainly from the burning of fossil fuels. In 2018, air pollution from fossil fuels caused $2.9 trillion in health and economic costs, about $8 billion a day.
+          Switching to clean sources of energy, such as wind and solar, thus helps address not only climate change but also air pollution and health.
+        </p>
+        <img src="https://swae-develop.s3.amazonaws.com/uiccatest.swaedev.com/webp/7af8e7e5-4d29-4681-ab39-19a4e2c34b75.webp" alt="proposal" />
+      </div>
+  )
+}
+
+export default ProposalInfo
