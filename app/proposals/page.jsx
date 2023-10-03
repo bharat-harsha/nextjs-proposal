d0e95b1c05ef2fd1ba7e378bba9e26f4995689fe
bharath-harsha <bharath.harsha@techpearl.com>
2023-10-03 15:43:52 +0530

    added head meta tag details

--- app/proposals/page.jsx
+++ app/proposals/page.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 const ProposalInfo = () => {
   return (
     <div>
-        <h1>Sustainable Renewable Energy</h1>
-        <p>According to the World Health Organization (WHO), about 99 percent of people in the world breathe air that exceeds air quality limits and threatens their health, and more than 13 million deaths around the world each year are due to avoidable environmental causes, including air pollution.
-          The unhealthy levels of fine particulate matter and nitrogen dioxide originate mainly from the burning of fossil fuels. In 2018, air pollution from fossil fuels caused $2.9 trillion in health and economic costs, about $8 billion a day.
-          Switching to clean sources of energy, such as wind and solar, thus helps address not only climate change but also air pollution and health.
-        </p>
-        <img src="https://swae-develop.s3.amazonaws.com/uiccatest.swaedev.com/webp/7af8e7e5-4d29-4681-ab39-19a4e2c34b75.webp" alt="proposal" />
-      </div>
+      <head>
+        <meta property="og:title" content="Sustainable Renewable Energy" />
+        <meta property="og:description" content="Switching to clean sources of energy helps address climate change, air pollution, and health issues." />
+        <meta property="og:image" content="https://swae-develop.s3.amazonaws.com/uiccatest.swaedev.com/webp/7af8e7e5-4d29-4681-ab39-19a4e2c34b75.webp" />
+        <meta property="og:url" content="https://proposal-22sjegb58-bharat-harshas-projects.vercel.app/proposals" />
+      </head>
+      <h1>Sustainable Renewable Energy</h1>
+      <p>According to the World Health Organization (WHO), about 99 percent of people in the world breathe air that exceeds air quality limits and threatens their health, and more than 13 million deaths around the world each year are due to avoidable environmental causes, including air pollution.
+        The unhealthy levels of fine particulate matter and nitrogen dioxide originate mainly from the burning of fossil fuels. In 2018, air pollution from fossil fuels caused $2.9 trillion in health and economic costs, about $8 billion a day.
+        Switching to clean sources of energy, such as wind and solar, thus helps address not only climate change but also air pollution and health.
+      </p>
+      <img src="https://swae-develop.s3.amazonaws.com/uiccatest.swaedev.com/webp/7af8e7e5-4d29-4681-ab39-19a4e2c34b75.webp" alt="proposal" />
+    </div>
   )
 }
 
